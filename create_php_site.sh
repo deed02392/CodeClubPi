20259d0283b32b11f9f403e257d2e39c89922014
George Hafiz <georgehafiz@gmail.com>
2014-05-23 19:59:21 +0100

    Remove PHP related entries. Adjust the vhost config template to remove caching of any content (the kids will be frequently changing the content).

--- create_php_site.sh
+++ create_php_site.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# @author: Seb Dangerfield
-# http://www.sebdangerfield.me.uk/?p=513 
+# @author: Seb Dangerfield <http://www.sebdangerfield.me.uk/?p=513>
+# @author: George Hafiz <georgehafiz-at-gmail-dot-com>
 # Created:   11/08/2011
-# Modified:   07/01/2012
-# Modified:   27/11/2012
 
 # Modify the following to match your system
 NGINX_CONFIG='/etc/nginx/sites-available'
 NGINX_SITES_ENABLED='/etc/nginx/sites-enabled'
-PHP_INI_DIR='/etc/php5/fpm/pool.d'
 WEB_SERVER_GROUP='www-data'
 NGINX_INIT='/etc/init.d/nginx'
-PHP_FPM_INIT='/etc/init.d/php5-fpm'
 # --------------END 
 SED=`which sed`
 CURRENT_DIR=`dirname $0`
@@ -37,13 +33,6 @@ echo "Please specify the username for this site?"
 read USERNAME
 HOME_DIR=$USERNAME
 adduser $USERNAME
-# -------
-# CentOS:
-# If you're using CentOS you will need to uncomment the next 3 lines!
-# -------
-#echo "Please enter a password for the user: $USERNAME"
-#read -s PASS
-#echo $PASS | passwd --stdin $USERNAME
 
 echo "Would you like to change to web root directory (y/n)?"
 read CHANGEROOT
@@ -63,25 +52,6 @@ $SED -i "s#@@PATH@@#\/home\/"$USERNAME$PUBLIC_HTML_DIR"#g" $CONFIG
 $SED -i "s/@@LOG_PATH@@/\/home\/$USERNAME\/_logs/g" $CONFIG
 $SED -i "s#@@SOCKET@@#/var/run/"$USERNAME"_fpm.sock#g" $CONFIG
 
-echo "How many FPM servers would you like by default:"
-read FPM_SERVERS
-echo "Min number of FPM servers would you like:"
-read MIN_SERVERS
-echo "Max number of FPM servers would you like:"
-read MAX_SERVERS
-# Now we need to create a new php fpm pool config
-FPMCONF="$PHP_INI_DIR/$DOMAIN.pool.conf"
-
-cp $CURRENT_DIR/pool.conf.template $FPMCONF
-
-$SED -i "s/@@USER@@/$USERNAME/g" $FPMCONF
-$SED -i "s/@@HOME_DIR@@/\/home\/$USERNAME/g" $FPMCONF
-$SED -i "s/@@START_SERVERS@@/$FPM_SERVERS/g" $FPMCONF
-$SED -i "s/@@MIN_SERVERS@@/$MIN_SERVERS/g" $FPMCONF
-$SED -i "s/@@MAX_SERVERS@@/$MAX_SERVERS/g" $FPMCONF
-MAX_CHILDS=$((MAX_SERVERS+START_SERVERS))
-$SED -i "s/@@MAX_CHILDS@@/$MAX_CHILDS/g" $FPMCONF
-
 usermod -aG $USERNAME $WEB_SERVER_GROUP
 chmod g+rx /home/$HOME_DIR
 chmod 600 $CONFIG
@@ -99,7 +69,6 @@ chmod 750 /home/$HOME_DIR$PUBLIC_HTML_DIR
 chown $USERNAME:$USERNAME /home/$HOME_DIR/ -R
 
 $NGINX_INIT reload
-$PHP_FPM_INIT restart
 
-echo -e "\nSite Created for $DOMAIN with PHP support"
+echo -e "\nSite Created for $DOMAIN"
 
